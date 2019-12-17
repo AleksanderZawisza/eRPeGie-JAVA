38bea426d1fd1ad4688b5553d43ac33ac36cd9f4
AleksanderZawisza <aleksanderpodsiad@wp.pl>
2019-12-17 18:22:48 +0100

    zmiany w walce

--- src/game/combat/Combat.java
+++ src/game/combat/Combat.java
@@ -1,12 +1,19 @@
 package game.combat;
 
 import game.creature.Creature;
-import game.creature.Player;
 
 public class Combat {
     public static int fight(Creature attacker, Creature attacked){
         attacked.lowerHp(  Math.max(attacker.getAttack()-attacked.getArmor(), 0 )  );
         return Math.max(attacker.getAttack()-attacked.getArmor(), 0 );  //ile attacker zadal obrazen
     }
+    public static int attack(Creature attacker, Creature attacked){
+        int potDamage = attacker.getAttack();
+        int armor = attacked.getArmor();
+        int actDamage = (int) Math.ceil(potDamage * (1 - (((double) armor )/ (armor + 50))));
+        attacked.lowerHp(actDamage);
+        return actDamage;
+    }
+
 
 }
