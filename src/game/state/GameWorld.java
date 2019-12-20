f6162b09d16005548932c4197b1408d1326e920c
AleksanderZawisza <aleksanderpodsiad@wp.pl>
2019-12-20 15:59:40 +0100

    drobny update

--- src/game/state/GameWorld.java
+++ src/game/state/GameWorld.java
@@ -42,7 +42,7 @@ public class GameWorld {
 
 
     public void opisPostaci(String savename) {
-        System.out.print("\nYour NAME is "+ savename + ". It is currently [DAY/WEATHER]. You are a [TRAIT], [TRAIT] [AGE] [GENDER].\n" +
+        System.out.print("\nYour NAME is [NAME]. It is currently [DAY/WEATHER]. You are a [TRAIT], [TRAIT] [AGE] [GENDER].\n" +
                 "You have a fondness for [HOBBY] and are an ASPIRING [JOB]. You like to [HOBBY] but are NOT VERY GOOD AT IT. \n" +
                 "You also enjoy KILLING THINGS sometimes.\n"); // generator postaci DO ZAIMPLEMENTOWANIA
     }
