cc2825dbe99143666c19f0480552bcf1bffd2310
z-mrozu <zmrozu@gmail.com>
2020-01-03 22:26:33 +0100

    EnemyGenerator:plains + deskryptory update II

--- src/game/creature/EnemyGenerator.java
+++ src/game/creature/EnemyGenerator.java
@@ -8,16 +8,16 @@ public class EnemyGenerator {
 
     public static ArrayList<Enemy> plains() {
         ArrayList<Enemy> plainsEnemies = new ArrayList<>();
-        plainsEnemies.add(new Enemy("mouse", 3,0,2,2,3));
-        plainsEnemies.add(new Enemy("rabbit", 5,0,3,3,10));
-        plainsEnemies.add(new Enemy("pheasant", 10,0,6,5,12));
-        plainsEnemies.add(new Enemy("goose", 9,0,10,5,12));
-        plainsEnemies.add(new Enemy("rat", 10,0,8,6,5));
-        plainsEnemies.add(new Enemy("snake", 20,1,16,9,8));
-        plainsEnemies.add(new Enemy("wild horse", 40,2,10,11,20));
-        plainsEnemies.add(new Enemy("badger", 30,5,22,13,25));
-        plainsEnemies.add(new Enemy("gaggle of geese", 50,1,18,15,30));
-        plainsEnemies.add(new Enemy("bison", 60,10,15,17,50));
+        plainsEnemies.add(new Enemy("mouse", 4,0,5,2,3));
+        plainsEnemies.add(new Enemy("rabbit", 5,0,7,3,10));
+        plainsEnemies.add(new Enemy("pheasant", 9,0,10,5,12));
+        plainsEnemies.add(new Enemy("goose", 10,0,13,5,12));
+        plainsEnemies.add(new Enemy("rat", 10,0,11,6,5));
+        plainsEnemies.add(new Enemy("snake", 20,1,19,9,8));
+        plainsEnemies.add(new Enemy("wild horse", 40,2,15,11,20));
+        plainsEnemies.add(new Enemy("badger", 30,5,27,13,25));
+        plainsEnemies.add(new Enemy("gaggle of geese", 50,1,23,15,30));
+        plainsEnemies.add(new Enemy("bison", 60,10,20,17,50));
 
         return plainsEnemies;
     }
@@ -29,10 +29,10 @@ public class EnemyGenerator {
     }
 
     public static Enemy descriptor(Enemy enemy) {
-        String[] desc = {"small", "weak", "diseased",  // -EXP, -ATT/HP
+        String[] desc = {"injured", "small", "diseased",  // -EXP, -ATT/HP
                 "completely average", "mediocre", "common", "typical", "plain", "boringly ordinary",
                 "shaggy", "rare", // -/+MONEY
-                "hardy", "large", "rabid", // +EXP, +ATT/ARMOR/HP
+                "large", "ferocious", "rabid", // +EXP, +ATT/ARMOR/HP
                 "massive",  // ++EXP, ++ATT, ++HP, +ARMOR, +MONEY
         };
 
@@ -48,19 +48,19 @@ public class EnemyGenerator {
 
         String myDesc = desc[rand.nextInt(desc.length)];
         switch(myDesc)
-        {   case "small": enemy.lowerHp((int) enemy.getMaxhp()/10); enemy.lowerExp(1); break;
-            case "weak": enemy.lowerAttack((int) enemy.getAttack()/10); enemy.lowerExp(1); break;
-            case "diseased": enemy.lowerHp((int) enemy.getMaxhp()/10);
-                             enemy.lowerAttack((int) enemy.getAttack()/10); enemy.lowerExp(1); break;
-            case "shaggy": enemy.lowerMoney((int) enemy.getMoney()/5); break;
-            case "rare": enemy.addMoney((int) enemy.getMoney()/5); break;
-            case "hardy": enemy.addHp((int) enemy.getMaxhp()/10); enemy.addExp(1);
+        {   case "injured": enemy.lowerHp((int) enemy.getMaxhp()/10 +2); enemy.lowerExp(1); break;
+            case "small": enemy.lowerAttack((int) enemy.getAttack()/10 +1); enemy.lowerExp(1); break;
+            case "diseased": enemy.lowerHp((int) enemy.getMaxhp()/10 +2);
+                             enemy.lowerAttack((int) enemy.getAttack()/10  +1); enemy.lowerExp(1); break;
+            case "shaggy": enemy.lowerMoney((int) enemy.getMoney()/5 +1); break;
+            case "rare": enemy.addMoney((int) enemy.getMoney()/5 +10); break;
+            case "large": enemy.addHp((int) enemy.getMaxhp()/10 +2); enemy.addExp(1);
                           enemy.addArmor((int) enemy.getArmor()/10); break;
-            case "large": enemy.addAttack((int) enemy.getAttack()/10); enemy.addExp(1); break;
-            case "rabid": enemy.addHp((int) enemy.getMaxhp()/10);
-                          enemy.addAttack((int) enemy.getAttack()/10); enemy.addExp(1); break;
-            case "massive": enemy.addHp((int) enemy.getMaxhp()/5); enemy.addArmor((int) enemy.getArmor()/5);
-                              enemy.addMoney((int) enemy.getMoney()/5); enemy.addAttack((int) enemy.getAttack()/5);
+            case "ferocious": enemy.addAttack((int) enemy.getAttack()/10 +1); enemy.addExp(1); break;
+            case "rabid": enemy.addHp((int) enemy.getMaxhp()/10 +2);
+                          enemy.addAttack((int) enemy.getAttack()/10 +1); enemy.addExp(1); break;
+            case "massive": enemy.addHp((int) enemy.getMaxhp()/5 +2); enemy.addArmor(1);
+                              enemy.addMoney((int) enemy.getMoney()/5 +5); enemy.addAttack((int) enemy.getAttack()/5 +1);
                               enemy.addExp(2); break;
             default: break;
         }
